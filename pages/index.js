6014e30f229ce6f5d9cf64764a36af50334f8cd9
Ali Siddique <alisiddique@Alis-MacBook-Air.local>
2022-09-19 10:43:24 +0100

    ni

--- pages/index.js
+++ pages/index.js
@@ -112,7 +112,7 @@ export default function Home() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-md sm:text-center">
           <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white animate-pulse">Sign up for our newsletter</h2>
-          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up with your email.</p>
+          <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Stay up to date with the tutorials you need, feel free to sign up with your email.</p>
           <div className="ml-form-embed mb-10"
           data-account="3736937:h9l7j8g0x8"
           data-form="5763401:v7d5h7">
