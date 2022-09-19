a241258c92f61a171322ef68f788c15d9fa897e4
Ali Siddique <alisiddique@Alis-MacBook-Air.local>
2022-09-19 10:38:35 +0100

    ome

--- pages/index.js
+++ pages/index.js
@@ -226,7 +226,7 @@ export default function Home() {
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Let's create more tools and ideas that brings us together.</h2>
             <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-black-400">Tutorial Heaven will lift you out of tutorial hell to practical tutorials and present you the best teachers in the world.</p>
             <Link href="/">
-            <a className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+            <a className="inline-flex items-center text-black bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                 Get started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </a>
@@ -244,7 +244,7 @@ export default function Home() {
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's get started!.</h2>
             <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Join our newsltter for more tutorials every week!.</p>
             <Link href="#news">
-            <a  className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+            <a  className="inline-flex items-center text-black bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                 Get started
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </a>
